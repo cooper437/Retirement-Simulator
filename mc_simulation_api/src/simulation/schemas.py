6ceb69307a6f6e2b376e9326737532d3a83ae54e
Sam Cooper <cooper437@gmail.com>
2021-12-21 12:54:42 -0500

    Moved a few more constants over to API parameters

--- mc_simulation_api/src/simulation/schemas.py
+++ mc_simulation_api/src/simulation/schemas.py
@@ -8,21 +8,29 @@ decimal_positive = condecimal(ge=0)
 
 
 class RunSimulationIn(BaseModel):
+    # Adjusts the portfolio balance each year both pre and post retirement based on the inflation_mean to reflect value in todays dollars
     adjust_portfolio_balance_for_inflation: bool
+    # Accounts for the wage_growth_mean in the annual amount contributed pre-retirement
     adjust_contributions_for_income_growth: bool
+    # Accounts for the wage_growth_mean in the annual amount contributed pre-retirement
     adjust_withdrawals_for_inflation: bool
+    # Accounts for the post_retirement_tax_rate in the annual amount withdrawn post-retirement
     adjust_withdrawals_for_taxation: bool
+    # Portfolio Parameters
     initial_portfolio_amount: PositiveInt
     pre_retirement_annual_contribution: PositiveInt
     post_retirement_annual_withdrawal: NegativeInt
+    # Lifestyle Parameters
     current_age: PositiveInt
     retirement_age: PositiveInt
     life_expectancy: PositiveInt
+    # Market Condition Parameters
     inflation_mean: decimal_positive
     income_growth_mean: decimal_positive
     pre_retirement_mean_rate_of_return: decimal_positive
     post_retirement_mean_rate_of_return: decimal_positive
     pre_retirement_rate_of_return_volatility: decimal_positive
     post_retirement_rate_of_return_volatility: decimal_positive
+    # Taxation
     post_retirement_tax_rate: decimal_positive
     additional_post_retirement_annual_income: decimal_positive
