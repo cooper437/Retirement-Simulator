f6f2894983bf01138b20c8500dce335b444a1d80
Sam Cooper <cooper437@gmail.com>
2021-12-22 14:23:50 -0500

    Added top nav

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import AppBar from './components/TopNavBar';
 
 export default function App() {
-  return <div className="App">TruWealth</div>;
+  return (
+    <div className="App">
+      <AppBar />
+    </div>
+  );
 }
