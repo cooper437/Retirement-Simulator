297dcd8c834e6de3d8ab41d956df2b4172be8ded
Sam Cooper <cooper437@gmail.com>
2021-12-23 09:25:14 -0500

    WIP

---
+++ ui/src/colors.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const SECTION_BACKGROUND_COLOR = 'rgba(231, 235, 240, 0.6)';
--- ui/src/components/McSimulatorForm/FormContainer.js
+++ ui/src/components/McSimulatorForm/FormContainer.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import LabeledFormSection from './LabeledFormSection';
 
 export default function FormContainer() {
   return (
     <Box sx={{ mt: 4 }}>
       <Box sx={{ pb: 4, borderBottom: '1px solid gray' }}>
         <Typography variant="h6">Monte Carlo Simulator</Typography>
-        <Typography>
+        <Typography variant="p">
           Input the information below and then run the simulation to see the
           likelihood that you will meet your retirement goals under a variety of
           market conditions
         </Typography>
       </Box>
+      <LabeledFormSection />
     </Box>
   );
 }
