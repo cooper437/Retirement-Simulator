8e9c9a7a47e3401a098bf87defd188f5a7800475
Sam Cooper <cooper437@gmail.com>
2022-01-03 14:39:33 -0500

    WIP

--- ui/src/components/McSimulatorForm/FormResults/ResultsContainer.js
+++ ui/src/components/McSimulatorForm/FormResults/ResultsContainer.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import {
-  Box,
-  Typography,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText
-} from '@mui/material';
-import CheckIcon from '@mui/icons-material/Check';
+import { Box, Typography } from '@mui/material';
 import SummaryStatsTable from './SummaryStatsTable';
 import { decimalToPercent } from '../../../utils/generalUtils';
+import KeyFactsList from './KeyFactsList';
 
 export default function ResultsContainer({ simulationResults }) {
   if (simulationResults.simulationRunCompleted) {
@@ -24,38 +17,11 @@ export default function ResultsContainer({ simulationResults }) {
         <Typography sx={{ mb: 2 }} variant="h6">
           Key Facts
         </Typography>
-        <List>
-          <ListItem>
-            <ListItemIcon>
-              <CheckIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary={`${simulationResults.numberOfSimulations} retirement scenarios were simulated.`}
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-              <CheckIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary={`In ${roundedSurvivalRate} of simulated scenarios your portfolio did not run out of money prior to life expectancy.`}
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-              <CheckIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary={`In ${roundedDepletionRate} of simulated scenarios your portfolio did run out of money prior to life expectancy.`}
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-              <CheckIcon />
-            </ListItemIcon>
-            <ListItemText primary="Post-retirement if you withdraw less than $XXX / year there is a 95% probability of not running out of money." />
-          </ListItem>
-        </List>
+        <KeyFactsList
+          simulationResults={simulationResults}
+          roundedSurvivalRate={roundedSurvivalRate}
+          roundedDepletionRate={roundedDepletionRate}
+        />
         <Typography sx={{ mb: 2, mt: 2 }} variant="h6">
           Summary Statistics
         </Typography>
