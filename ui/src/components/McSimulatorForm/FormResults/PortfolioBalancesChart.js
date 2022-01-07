0beb60bd8c6c032b24c68b0ab99b8067d441c219
Sam Cooper <cooper437@gmail.com>
2022-01-07 09:34:10 -0500

    WIP

--- ui/src/components/McSimulatorForm/FormResults/PortfolioBalancesChart.js
+++ ui/src/components/McSimulatorForm/FormResults/PortfolioBalancesChart.js
@@ -13,6 +13,26 @@ import {
 import { Line } from 'react-chartjs-2';
 import { numberWithCommas } from '../../../utils/generalUtils';
 
+export const CHART_COLORS = {
+  red: ['rgb(255, 99, 132)', 'rgba(255, 99, 132, 0.5)'],
+  orange: ['rgb(255, 159, 64)', 'rgba(255, 159, 64, 0.5)'],
+  yellow: ['rgb(255, 205, 86)', 'rgba(255, 205, 86, 0.5)'],
+  green: ['rgb(75, 192, 192)', 'rgba(75, 192, 192, 0.5)'],
+  blue: ['rgb(54, 162, 235)', 'rgba(54, 162, 235, 0.5)'],
+  purple: ['rgb(153, 102, 255)', 'rgba(153, 102, 255, 0.5)'],
+  grey: ['rgb(201, 203, 207)', 'rgba(201, 203, 207, 0.5)']
+};
+
+const NAMED_COLORS = [
+  CHART_COLORS.red,
+  CHART_COLORS.orange,
+  CHART_COLORS.yellow,
+  CHART_COLORS.green,
+  CHART_COLORS.blue,
+  CHART_COLORS.purple,
+  CHART_COLORS.grey
+];
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -61,12 +81,14 @@ const constructChartData = (quantileStatistics) => {
   const firstAvailableQuantile = Object.keys(quantileStatistics)[0];
   const numYears = quantileStatistics[firstAvailableQuantile].balances.length;
   const labels = [...Array(numYears).keys()].map((i) => i + currentYear);
-  const datasets = Object.entries(quantileStatistics).map(([key, value]) => ({
-    label: key,
-    data: value.balances,
-    borderColor: 'rgb(255, 99, 132)',
-    backgroundColor: 'rgba(255, 99, 132, 0.5)'
-  }));
+  const datasets = Object.entries(quantileStatistics).map(
+    ([key, value], index) => ({
+      label: key,
+      data: value.balances,
+      borderColor: NAMED_COLORS[index][0],
+      backgroundColor: NAMED_COLORS[index][1]
+    })
+  );
   const data = {
     labels,
     datasets
