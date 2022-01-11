f97459e784b16e7e147b2632deed6c288608e7cd
Sam Cooper <cooper437@gmail.com>
2022-01-11 12:11:20 -0500

    WIP

--- ui/src/components/McSimulatorForm/FormSections/IncomeFormContent.js
+++ ui/src/components/McSimulatorForm/FormSections/IncomeFormContent.js
@@ -33,68 +33,72 @@ export default function IncomeFormContent({
 }) {
   return (
     <>
-      {' '}
-      <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
-        <FormControl sx={commonFormStyles.shortFormInput}>
-          <TextField
-            label="Post-Retirement Rate of Return"
-            variant="outlined"
-            value={postRetirementMeanRateOfReturn}
-            onChange={handleChange}
-            name="postRetirementMeanRateOfReturn"
-            id="post-retirement-ror"
-            InputLabelProps={{
-              shrink: true
-            }}
-            InputProps={{
-              inputComponent: NumberFormatPercentage,
-              endAdornment: <InputAdornment position="end">%</InputAdornment>
-            }}
-            error={
-              touched.postRetirementMeanRateOfReturn &&
-              Boolean(errors.postRetirementMeanRateOfReturn)
-            }
-            helperText={
-              touched.postRetirementMeanRateOfReturn &&
-              errors.postRetirementMeanRateOfReturn
-            }
-          />
-        </FormControl>
-      </Box>
       <Box
         display="flex"
         flexDirection="row"
         justifyContent="space-between"
         flexWrap="wrap"
       >
-        <FormControl sx={commonFormStyles.shortFormInput}>
-          <TextField
-            label="Post-Retirement Additional Annual Income"
-            variant="outlined"
-            value={additionalPostRetirementAnnualIncome}
-            onChange={handleChange}
-            name="additionalPostRetirementAnnualIncome"
-            id="additional-annual-income"
-            InputLabelProps={{
-              shrink: true
-            }}
-            InputProps={{
-              inputComponent: NumberFormatDollarAmount,
-              startAdornment: (
-                <InputAdornment position="start">$</InputAdornment>
-              ),
-              endAdornment: <InputAdornment position="end">.00</InputAdornment>
-            }}
-            error={
-              touched.additionalPostRetirementAnnualIncome &&
-              Boolean(errors.additionalPostRetirementAnnualIncome)
-            }
-            helperText={
-              touched.additionalPostRetirementAnnualIncome &&
-              errors.additionalPostRetirementAnnualIncome
-            }
-          />
-        </FormControl>
+        <Box sx={{ flex: 1 }}>
+          <FormControl sx={commonFormStyles.shortFormInput}>
+            <TextField
+              label="Post-Retirement Rate of Return"
+              variant="outlined"
+              value={postRetirementMeanRateOfReturn}
+              onChange={handleChange}
+              name="postRetirementMeanRateOfReturn"
+              id="post-retirement-ror"
+              InputLabelProps={{
+                shrink: true
+              }}
+              InputProps={{
+                inputComponent: NumberFormatPercentage,
+                endAdornment: <InputAdornment position="end">%</InputAdornment>
+              }}
+              error={
+                touched.postRetirementMeanRateOfReturn &&
+                Boolean(errors.postRetirementMeanRateOfReturn)
+              }
+              helperText={
+                touched.postRetirementMeanRateOfReturn &&
+                errors.postRetirementMeanRateOfReturn
+              }
+            />
+          </FormControl>
+        </Box>
+        <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+          <FormControl sx={commonFormStyles.shortFormInput}>
+            <TextField
+              label="Post-Retirement Additional Annual Income"
+              variant="outlined"
+              value={additionalPostRetirementAnnualIncome}
+              onChange={handleChange}
+              name="additionalPostRetirementAnnualIncome"
+              id="additional-annual-income"
+              InputLabelProps={{
+                shrink: true
+              }}
+              InputProps={{
+                inputComponent: NumberFormatDollarAmount,
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">.00</InputAdornment>
+                )
+              }}
+              error={
+                touched.additionalPostRetirementAnnualIncome &&
+                Boolean(errors.additionalPostRetirementAnnualIncome)
+              }
+              helperText={
+                touched.additionalPostRetirementAnnualIncome &&
+                errors.additionalPostRetirementAnnualIncome
+              }
+            />
+          </FormControl>
+        </Box>
+        <Box sx={{ flex: 1 }} />
       </Box>
       <Box
         display="flex"
@@ -104,6 +108,83 @@ export default function IncomeFormContent({
         height="72px"
         flexWrap="wrap"
       >
+        <Box sx={{ flex: 1 }}>
+          <FormControl sx={commonFormStyles.shortFormInput}>
+            <TextField
+              label="Annual Net Withdrawal Post-Retirement"
+              variant="outlined"
+              value={postRetirementAnnualWithdrawal}
+              onChange={handleChange}
+              name="postRetirementAnnualWithdrawal"
+              id="annual-withdrawal-post-retirement"
+              InputLabelProps={{
+                shrink: true
+              }}
+              InputProps={{
+                inputComponent: NumberFormatDollarAmount,
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">.00</InputAdornment>
+                )
+              }}
+              error={
+                touched.postRetirementAnnualWithdrawal &&
+                Boolean(errors.postRetirementAnnualWithdrawal)
+              }
+              helperText={
+                touched.postRetirementAnnualWithdrawal &&
+                errors.postRetirementAnnualWithdrawal
+              }
+            />
+          </FormControl>
+        </Box>
+        <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+          <FormControl
+            sx={commonFormStyles.shortFormInput}
+            error={
+              touched.postRetirementInvestmentStyle &&
+              Boolean(errors.postRetirementInvestmentStyle)
+            }
+          >
+            <InputLabel id="post-retirement-investment-style-label">
+              Post-Retirement Investment Style
+            </InputLabel>
+            <Select
+              labelId="post-retirement-investment-style-label"
+              id="post-retirement-investment-style"
+              label="Post-Retirement Investment Style"
+              startAdornment={<InputAdornment position="start" />}
+              endAdornment={<InputAdornment position="end" />}
+              value={postRetirementInvestmentStyle}
+              name="postRetirementInvestmentStyle"
+              onChange={async (e) => {
+                const selection = Object.values(INVESTMENT_STYLE_ENUM).find(
+                  (i) => i.label === e.target.value
+                );
+                await handleChange(e);
+                setFieldValue(
+                  'postRetirementMeanRateOfReturn',
+                  selection.postRetirementMeanRateOfReturn
+                );
+              }}
+            >
+              {Object.values(INVESTMENT_STYLE_ENUM).map((i) => (
+                <MenuItem key={i.value} value={i.label}>
+                  {i.label}
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>
+              {touched.postRetirementInvestmentStyle &&
+                errors.postRetirementInvestmentStyle}
+            </FormHelperText>
+          </FormControl>
+        </Box>
+        <Box sx={{ flex: 1 }} />
+      </Box>
+      <Box sx={{ display: 'flex' }}>
         <Box sx={{ flex: 1 }}>
           <FormGroup>
             <FormControlLabel
@@ -119,103 +200,33 @@ export default function IncomeFormContent({
           </FormGroup>
         </Box>
         {adjustForInflation && (
-          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
-            <FormControl sx={commonFormStyles.shortFormInput}>
-              <TextField
-                label="Annual Inflation Mean"
-                variant="outlined"
-                value={inflationMean}
-                name="inflationMean"
-                onChange={handleChange}
-                id="inflation-mean"
-                InputLabelProps={{
-                  shrink: true
-                }}
-                InputProps={{
-                  inputComponent: NumberFormatPercentage,
-                  endAdornment: (
-                    <InputAdornment position="end">%</InputAdornment>
-                  )
-                }}
-                error={touched.inflationMean && Boolean(errors.inflationMean)}
-                helperText={touched.inflationMean && errors.inflationMean}
-              />
-            </FormControl>
-          </Box>
+          <>
+            <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+              <FormControl sx={commonFormStyles.shortFormInput}>
+                <TextField
+                  label="Annual Inflation Mean"
+                  variant="outlined"
+                  value={inflationMean}
+                  name="inflationMean"
+                  onChange={handleChange}
+                  id="inflation-mean"
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  InputProps={{
+                    inputComponent: NumberFormatPercentage,
+                    endAdornment: (
+                      <InputAdornment position="end">%</InputAdornment>
+                    )
+                  }}
+                  error={touched.inflationMean && Boolean(errors.inflationMean)}
+                  helperText={touched.inflationMean && errors.inflationMean}
+                />
+              </FormControl>
+            </Box>
+            <Box sx={{ flex: 1 }} />
+          </>
         )}
-        <Box sx={{ flex: 1 }} />
-      </Box>
-      <Box>
-        <FormControl sx={commonFormStyles.shortFormInput}>
-          <TextField
-            label="Annual Net Withdrawal Post-Retirement"
-            variant="outlined"
-            value={postRetirementAnnualWithdrawal}
-            onChange={handleChange}
-            name="postRetirementAnnualWithdrawal"
-            id="annual-withdrawal-post-retirement"
-            InputLabelProps={{
-              shrink: true
-            }}
-            InputProps={{
-              inputComponent: NumberFormatDollarAmount,
-              startAdornment: (
-                <InputAdornment position="start">$</InputAdornment>
-              ),
-              endAdornment: <InputAdornment position="end">.00</InputAdornment>
-            }}
-            error={
-              touched.postRetirementAnnualWithdrawal &&
-              Boolean(errors.postRetirementAnnualWithdrawal)
-            }
-            helperText={
-              touched.postRetirementAnnualWithdrawal &&
-              errors.postRetirementAnnualWithdrawal
-            }
-          />
-        </FormControl>
-      </Box>
-      <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
-        <FormControl
-          sx={commonFormStyles.shortFormInput}
-          error={
-            touched.postRetirementInvestmentStyle &&
-            Boolean(errors.postRetirementInvestmentStyle)
-          }
-        >
-          <InputLabel id="post-retirement-investment-style-label">
-            Post-Retirement Investment Style
-          </InputLabel>
-          <Select
-            labelId="post-retirement-investment-style-label"
-            id="post-retirement-investment-style"
-            label="Post-Retirement Investment Style"
-            startAdornment={<InputAdornment position="start" />}
-            endAdornment={<InputAdornment position="end" />}
-            value={postRetirementInvestmentStyle}
-            name="postRetirementInvestmentStyle"
-            onChange={async (e) => {
-              const selection = Object.values(INVESTMENT_STYLE_ENUM).find(
-                (i) => i.label === e.target.value
-              );
-              await handleChange(e);
-              setFieldValue(
-                'postRetirementMeanRateOfReturn',
-                selection.postRetirementMeanRateOfReturn
-              );
-            }}
-          >
-            {Object.values(INVESTMENT_STYLE_ENUM).map((i) => (
-              <MenuItem key={i.value} value={i.label}>
-                {i.label}
-              </MenuItem>
-            ))}
-          </Select>
-          <FormHelperText>
-            {touched.postRetirementInvestmentStyle &&
-              errors.postRetirementInvestmentStyle}
-          </FormHelperText>
-        </FormControl>
       </Box>
     </>
   );
