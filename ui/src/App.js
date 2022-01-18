b5fc3773d5ad634f3d2341e70d09a1680578a8c9
Sam Cooper <cooper437@gmail.com>
2022-01-18 14:13:27 -0500

    Added routing

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Container } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import AppBar from './components/TopNavBar';
 import FormContainer from './components/McSimulatorForm/FormContainer';
+import PlanningStyleChooser from './components/PlanningQuestionnaire/PlanningStyleChooser';
 
 export default function App() {
   return (
     <div className="App">
       <AppBar />
       <Container>
-        <FormContainer />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<PlanningStyleChooser />} />
+          <Route path="/mc-input-form" element={<FormContainer />} />
+        </Routes>
       </Container>
     </div>
   );
