ef819a42bd3a16a522fa573dafd4ee389cb4b7d1
Sam Cooper <cooper437@gmail.com>
2022-01-20 12:27:07 -0500

    WIP

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Container, Box } from '@mui/material';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import AppBar from './components/TopNavBar';
 import FormContainer from './components/McSimulatorForm/FormContainer';
 import PlanningStyleChooser from './components/PlanningQuestionnaire/PlanningStyleChooser';
-import QuestionnaireStepOne from './components/PlanningQuestionnaire/QuestionnaireStepOne';
 import QuestionnaireStateManager from './components/PlanningQuestionnaire/QuestionnaireStateManager';
 
 export default function App() {
+  const location = useLocation();
+  const getBackgroundColor = (locationPath) => {
+    if (locationPath && locationPath === '/mc-input-form') return 'white';
+    return 'rgb(231, 235, 240)';
+  };
   return (
-    <Box className="App">
+    <Box
+      sx={{ backgroundColor: getBackgroundColor(location.pathname) }}
+      className="App"
+    >
       <AppBar />
       <Container>
         <Routes>
